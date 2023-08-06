5e74a530d950d2bc6ba2829c8633686bc27c613f
Alexey Stypanets <astep2004@gmail.com>
2023-08-06 17:06:13 +0300

    create connection 2 users to 1 room.

--- public/main.js
+++ public/main.js
@@ -1,33 +1,84 @@
 const socket = io();
 
 // main elements
+const chatTitle = document.getElementById('chat-title');
 const messageForm = document.getElementById('send-container');
 const messageInput = document.getElementById('message-input');
 const sendButton = document.getElementById('send-button');
 const messageContainer = document.getElementById('message-container');
+const usersContainer = document.getElementById('users-container');
+const chatContainer = document.getElementById('chat-container');
+
+let socketID;
+let userName = '';
+let roomID = '';
+const currentUser = undefined;
 
 //authentication
 const nameInput = document.getElementById('name-input');
-let userName = '';
 
 //receive from server information
 // listen each message and add it to the socket
+
 socket.on('users-count', (data) => {
-  console.log(data);
+  upgradeUserList(data);
 });
-socket.on('user-connected', (user) => {
-  appendMessage(`${user.name} connected`);
+
+socket.on('receive-msg', (message) => {
+  appendMessage(message);
 });
-socket.on('chat-message', (data) => {
-  const { message, user } = data;
-  appendMessage(`${user.name}: ${message}`);
+
+socket.on('clear-msgs', () => {
+  messageContainer.innerHTML = '';
 });
+
+/* UPGRADE USERS LIST */
+function upgradeUserList(users) {
+  console.log(users);
+  socketID = socket.id;
+  usersContainer.innerHTML = '';
+  let currentUser = users.find((user) => user.id === socketID);
+  let roomID = currentUser.room.id;
+
+  /* ADD USERS OF ROOM */
+  users
+    .filter((user) => user.id !== socketID)
+    .map((user) => {
+      const userElement = document.createElement('div');
+      userElement.classList.add('user-item');
+      userElement.innerText = `${user.name}`;
+
+      if (user.room.id === roomID) {
+        if (user.joined) {
+          userElement.classList.add('user-joined');
+        }
+        userElement.addEventListener('click', () => {
+          socket.emit('close-user', roomID);
+        });
+        usersContainer.append(userElement);
+      }
+
+      if (!user.joined && user.room.id !== roomID) {
+        userElement.addEventListener('click', () => {
+          const params = { userID: user.id, roomID: roomID };
+          console.log(params);
+          socket.emit('join-user', params);
+          chatTitle.innerText = `Chat with ${userName} and ${user.name}`;
+        });
+        usersContainer.append(userElement);
+      }
+    });
+}
+
+// socket.on('chat-message', (data) => {
+//   const { message, user } = data;
+//   appendMessage(`${user.name}: ${message}`);
+// });
 
 messageForm.addEventListener('submit', (e) => {
   e.preventDefault();
   if (messageInput.value) {
     const message = messageInput.value;
-    appendMessage(`You: ${message}`);
     socket.emit('send-chat-msg', message);
     messageInput.value = '';
   }
@@ -59,8 +110,8 @@ nameInput.addEventListener('keyup', (e) => {
     nameInput.style.display = 'none';
     // nameInput.value = '';
     socket.emit('new-user', userName);
-    messageContainer.style.display = '';
-    messageForm.style.display = '';
+    chatContainer.style.display = '';
+
     messageInput.focus();
     appendMessage(`Wellcome, ${userName}! You joined`);
   }
--- server.js
+++ server.js
@@ -10,44 +10,99 @@ const io = require('socket.io')(server);
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Socket connections
-let activeUsers = new Set();
 let users = [];
-
 //Socket connections
 io.on('connection', onConnected);
 
 //Socket connections
 function onConnected(socket) {
-  activeUsers.add(socket.id);
-
-  //new-user
+  /* NEW USER*/
   socket.on('new-user', (userName) => {
     const user = {};
     user.id = socket.id;
     user.name = userName;
     user.role = 'new-user';
-    user.active = true;
+    user.joined = false;
+    user.room = { id: socket.id, name: userName };
+
+    const roomId = socket.id;
+    socket.join(roomId);
+    console.log(socket);
+
     users.push(user);
-    io.emit('users-count', users);
-    socket.broadcast.emit('user-connected', user);
+    // socket.broadcast.emit('user-connected', user);
+    upgradeUserListClient();
   });
+
+  /*INVITE OTHER USER */
+  socket.on('join-user', (data) => {
+    const { userID, roomID } = data;
+    users
+      .filter((user) => user.room.id === roomID)
+      .map((user) => {
+        user.joined = false;
+        user.room = { id: user.id, name: user.name };
+      });
+    const userJoin = getUser(userID);
+    const userOwner = getUser(socket.id);
+
+    userJoin.room = { id: userOwner['id'], name: userOwner['name'] };
+    userJoin.joined = true;
+    userOwner.joined = true;
+
+    clearMessages(roomID);
+
+    upgradeUserListClient();
+  });
+
+  /* JOIN TO ROOM USER */
+  socket.on('close-user', (roomID) => {
+    users
+      .filter((user) => user.room.id === roomID)
+      .map((user) => {
+        user.joined = false;
+        user.room = { id: user.id, name: user.name };
+      });
+
+    clearMessages(roomID);
+    upgradeUserListClient();
+  });
+
   // receive message
   socket.on('send-chat-msg', (message) => {
-    //console.log(message);
     //send msg to other users
-    //io.emit('chat-message', message);
-    const user = getUser(socket.id);
-    socket.broadcast.emit('chat-message', { message, user });
-    //io.emit('send-chat-msg', message);
+    const userFrom = getUser(socket.id);
+    const roomID = userFrom.room.id;
+    const usersRoom = users.filter((user) => user.room.id === roomID);
+
+    if (usersRoom.length === 2) {
+      const userTO = usersRoom.filter((user) => user.id !== socket.id)[0];
+      console.log(userTO);
+
+      sendMessage(userFrom, userTO, message);
+    }
+    upgradeUserListClient();
   });
 
   socket.on('disconnect', () => {
     users = users.filter((user) => user.id !== socket.id);
-    io.emit('users-count', users);
+    upgradeUserListClient();
   });
+}
+
+function upgradeUserListClient() {
+  io.emit('users-count', users);
 }
 
 function getUser(socketID) {
   const user = users.find((user) => user.id === socketID);
   return user;
+}
+
+function sendMessage(userFrom, userTo, msg) {
+  io.to(userTo.room.id).emit('receive-msg', `${userFrom.name}: ${msg}`);
+}
+
+function clearMessages(roomID) {
+  io.to(roomID).emit('clear-msgs');
 }
